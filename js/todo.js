bccb9daf04e2d3a9d29fa178ae04f732572ecebe
DmitryTruten <dmitriy2503a@gmail.com>
2022-09-02 16:31:09 +0300

    added id to each delete button

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,15 @@
 const todos = [{
   title: "Get groceries",
   dueDate: "04-09-2022",
+  id: 'id1'
 }, {
   title: "Wash car",
   dueDate:" 04-09-2022",
+  id: 'id2'
 }, {
   title: "Make dinner",
   dueDate: "04-09-2022",
+  id: 'id3'
 }]
 
 render()
@@ -17,12 +20,23 @@ function addNewTodo(){
 
   const datePicker = document.getElementById('date-picker')
   const dueDate= datePicker.value;
+
+  const id = new Date().getTime();
+
+
   todos.push({
     title: title,
     dueDate: dueDate,
+    id: id
   })
 
   render()
+}
+
+function deleteTodo(event) {
+  const deleteButton = event.target
+  const idToDelete = deleteButton.id
+  
 }
 
 function render() {
@@ -30,9 +44,18 @@ function render() {
   document.getElementById('todo-list').innerHTML = ''
 
   todos.forEach(function(todo) {
-    const todoList = document.getElementById('todo-list')
+    
     const element = document.createElement('div');
     element.innerText = todo.title + ' ' + todo.dueDate;
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'Delete';
+    deleteButton.style = 'margin-left: 20px;'
+    deleteButton.onclick = deleteTodo;
+    deleteButton.id = todo.id
+    element.appendChild(deleteButton)
+
+    const todoList = document.getElementById('todo-list')
     todoList.appendChild(element);
   })
 }
