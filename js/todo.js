ed331fd867b1fb46aa9c217d6b2145fbaf205497
DmitryTruten <dmitriy2503a@gmail.com>
2022-09-02 11:49:56 +0300

    added date object for todo list

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,27 @@
-const todos = ["Get groceries", "Wash car", "Make dinner"]
+const todos = [{
+  title: "Get groceries",
+  dueDate: "04-09-2022",
+}, {
+  title: "Wash car",
+  dueDate:" 04-09-2022",
+}, {
+  title: "Make dinner",
+  dueDate: "04-09-2022",
+}]
 
 render()
 
 function addNewTodo(){
   const textbox = document.getElementById('todo-title')
   const title = textbox.value;
-  todos.push(title)
+
+  const datePicker = document.getElementById('date-picker')
+  const dueDate= datePicker.value;
+  todos.push({
+    title: title,
+    dueDate: dueDate,
+  })
+
   render()
 }
 
@@ -13,10 +29,10 @@ function render() {
   //reset list
   document.getElementById('todo-list').innerHTML = ''
 
-  todos.forEach(function(todoTitle) {
+  todos.forEach(function(todo) {
     const todoList = document.getElementById('todo-list')
     const element = document.createElement('div');
-    element.innerText = todoTitle;
+    element.innerText = todo.title + ' ' + todo.dueDate;
     todoList.appendChild(element);
   })
 }
