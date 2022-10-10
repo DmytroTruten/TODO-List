694cf4e9873d84545fb70a20267508b56b7f47b9
DmitryTruten <dmitriy2503a@gmail.com>
2022-10-10 13:28:47 +0300

    fixed bug with colorPalette display

--- js/todo.js
+++ js/todo.js
@@ -4,42 +4,46 @@
 let todos;
 
 // Retrieve localStorage
-const savedTodos = JSON.parse(localStorage.getItem('todos'));
+const savedTodos = JSON.parse(localStorage.getItem("todos"));
 // Check if it's an array
 if (Array.isArray(savedTodos)) {
   todos = savedTodos;
 } else {
-  todos = [{
-    title: 'Get groceries',
-    dueDate: '2021-10-04',
-    id: 'id1'
-  }, {
-    title: 'Wash car',
-    dueDate: '2021-02-03',
-    id: 'id2'
-  }, {
-    title: 'Make dinner',
-    dueDate: '2021-03-04',
-    id: 'id3'
-  }];
+  todos = [
+    {
+      title: "Get groceries",
+      dueDate: "2021-10-04",
+      id: "id1",
+    },
+    {
+      title: "Wash car",
+      dueDate: "2021-02-03",
+      id: "id2",
+    },
+    {
+      title: "Make dinner",
+      dueDate: "2021-03-04",
+      id: "id3",
+    },
+  ];
 }
 
 // Creates a todo
 const createTodo = (title, dueDate) => {
-  const id = '' + new Date().getTime();
+  const id = "" + new Date().getTime();
 
   todos.push({
     title: title,
     dueDate: dueDate,
-    id: id
+    id: id,
   });
 
   saveTodos();
-}
+};
 
 // Deletes a todo
-const removeTodo = idToDelete => {
-  todos = todos.filter(todo => {
+const removeTodo = (idToDelete) => {
+  todos = todos.filter((todo) => {
     // If the id of this todo matches idToDelete, return false
     // For everything else, return true
     if (todo.id === idToDelete) {
@@ -50,80 +54,82 @@ const removeTodo = idToDelete => {
   });
 
   saveTodos();
-}
+};
 
 const saveTodos = () => {
-  localStorage.setItem('todos', JSON.stringify(todos));
-}
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
 // Controller
 const addTodo = () => {
-  const textbox = document.getElementById('todo-title');
+  const textbox = document.getElementById("todo-title");
   const title = textbox.value;
-  if(title.includes(' ')) {
-    const datePicker = document.getElementById('date-picker');
+  if (title.includes(" ")) {
+    const datePicker = document.getElementById("date-picker");
     const dueDate = datePicker.value;
     createTodo(title, dueDate);
     render();
   }
-}
+};
 
-const deleteTodo = event => {
+const deleteTodo = (event) => {
   const deleteButton = event.target;
   const idToDelete = deleteButton.id;
   removeTodo(idToDelete);
   render();
-}
+};
 
-const upperContainer = document.getElementById('upper');
+const upperContainer = document.getElementById("upper");
+const toggleButton = document.getElementById("color-toggle");
+const colorPalette = document.getElementById("color-palette");
+colorPalette.style.display = "none";
 
 const toggleColorContainer = () => {
-  const toggleButton = document.getElementById('color-toggle');
-  const colorPalette = document.getElementById('color-palette');
-  if(toggleButton.value === 'off') {
+  if (toggleButton.value === "off") {
     upperContainer.classList.toggle("reveal-colors");
-    toggleButton.value = 'on'
+    toggleButton.value = "on";
     colorPalette.style.display = "grid";
   } else {
-    upperContainer.classList.remove("reveal-colors")
-    toggleButton.value = 'off';
+    upperContainer.classList.remove("reveal-colors");
+    toggleButton.value = "off";
     colorPalette.style.display = "none";
-}}
+  }
+};
 
 const toggleColors = (event) => {
-  if(event === red) {
-    upperContainer.classList.toggle('upper-red');
-  } else if(event === blue) {
-    upperContainer.classList.toggle('upper-blue');
+  if (event === red) {
+    upperContainer.classList.toggle("upper-red");
+  } else if (event === blue) {
+    upperContainer.classList.toggle("upper-blue");
   } else {
-    upperContainer.classList.toggle('upper-green');
+    upperContainer.classList.toggle("upper-green");
   }
-}
+};
 
 // View
 const render = () => {
   // reset our list
-  document.getElementById('todo-list').innerHTML = '';
+  document.getElementById("todo-list").innerHTML = "";
 
-  todos.forEach(todo => {
-    const checkbox = document.createElement('input');
-    checkbox.setAttribute("type", 'checkbox');
-    checkbox.classList.toggle('check')
+  todos.forEach((todo) => {
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.classList.toggle("check");
 
-    const element = document.createElement('div');
-    
-    element.innerText = todo.title + ' ' + todo.dueDate;
-    element.prepend(checkbox)
-    
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Delete';
+    const element = document.createElement("div");
+
+    element.innerText = todo.title + " " + todo.dueDate;
+    element.prepend(checkbox);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
     deleteButton.onclick = deleteTodo;
     deleteButton.id = todo.id;
     element.appendChild(deleteButton);
 
-    const todoList = document.getElementById('todo-list');
+    const todoList = document.getElementById("todo-list");
     todoList.appendChild(element);
   });
-}
+};
 
 render();
